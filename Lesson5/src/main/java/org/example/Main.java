1f8814701867bc2644e58a24d43e5df2fa6831ff
AnastasiyaBoyarin <anastasiya.boyarin@mail.ru>
2023-06-08 19:24:11 +0200

    Lesson5.
    Реализуйте структуру телефонной книги с помощью HashMap.
    Программа также должна учитывать, что во входной структуре будут повторяющиеся имена с разными телефонами, их необходимо считать, как одного человека с разными телефонами.
    Вывод должен быть отсортирован по убыванию числа телефонов.

---
+++ Lesson5/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Task1
+        Task1 task1 = new Task1();
+        task1.createPhoneBook();
+        task1.sortByCountOfNumbers();
+    }
+}
